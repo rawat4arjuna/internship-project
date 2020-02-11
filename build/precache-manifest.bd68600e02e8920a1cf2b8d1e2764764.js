3b6dd4c770ab0fa7288c3d6db38e6a56a176558e
Arjun singh rawat <arjunsinghrawatmca18@gmail.com>
2020-02-11 23:56:52 +0530

    done

---
+++ build/precache-manifest.bd68600e02e8920a1cf2b8d1e2764764.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb99fdcb8f6bb901abd2204e50a642d",
+    "url": "/internship-project/index.html"
+  },
+  {
+    "revision": "6c7bbd1b281670347320",
+    "url": "/internship-project/static/js/2.83f9ac27.chunk.js"
+  },
+  {
+    "revision": "1d59cd014babf6be5ef15a3426f53fc6",
+    "url": "/internship-project/static/js/2.83f9ac27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bef47776237b1af8508",
+    "url": "/internship-project/static/js/main.6c712223.chunk.js"
+  },
+  {
+    "revision": "644ca1a8f4ceb3bb3a8c",
+    "url": "/internship-project/static/js/runtime-main.3b95d3a1.js"
+  }
+]);
--- src/screen/login.js
+++ src/screen/login.js
@@ -27,9 +27,9 @@ async loginSubmit()
     
       if(this.ValidateEmail()  && this.ValidatePassword()) 
       {
-       const res = await  login (data)
+       const res = await  login (data).catch(console.error)
        console.warn("data",res)
-       if(res !== null)
+       if(typeof res !== "undefined")
        {
         if(res.password === password && res.user_email===user_email)
         {
@@ -37,7 +37,7 @@ async loginSubmit()
           localStorage.setItem("password",password)
           if(!res.verified)
           {
-            sendmail()
+            sendmail().catch(console.error)
             
          this.props.history.push('auth')
 
@@ -49,6 +49,9 @@ async loginSubmit()
  
         }
 
+       }
+       else{
+         console.warn("login Failed Network Error")
        }
        
        
